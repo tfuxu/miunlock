3456557a0698dcc26476e85cda42451b6d852d14
tfuxu <73042332+tfuxu@users.noreply.github.com>
2022-08-29 20:27:54 +0200

    feat: Update strings, and include more conditions to response checkings * remove unused imports * format source code with autopep8

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
-import logging, hashlib, io, time
+import logging
+import hashlib
+import io
+import time
+
 from request import Auth, UnlockRequest
+
 
 logging.basicConfig(level=logging.WARNING)
 
 use_fastboot = False
+
 
 try:
     from adb.fastboot import FastbootCommands
@@ -17,31 +23,38 @@ try:
     except DeviceNotFoundError:
         logging.warning("No device detected in fastboot. Using manual mode.")
 except ImportError:
-    logging.warning("python-adb fastboot interface unavailable. Using manual mode.")
+    logging.warning(
+        "python-adb fastboot interface unavailable. Using manual mode.")
+
 
 auth = Auth()
 auth.login_tui("unlockApi")
 logging.debug(auth.__dict__)
 
+
 r = UnlockRequest(auth, "unlock.update.miui.com", "/api/v3/unlock/userinfo", {
-  "data":{
-    "uid":auth.userid,
-    "clientId":"1",
-    "clientVersion":"6.5.406.31",
-    "language":"en",
-    "pcId":auth.pcid,
-    "region":"",
-}})
+    "data": {
+        "uid": auth.userid,
+        "clientId": "1",
+        "clientVersion": "6.5.406.31",
+        "language": "en",
+        "pcId": auth.pcid,
+        "region": "",
+    }})
 r.add_nonce()
 data = r.run()
 logging.debug(data)
+
+
 if not data["shouldApply"]:
-    logging.error("Xiaomi server says shouldApply false, status %d", data["applyStatus"])
+    logging.error(
+        "Xiaomi server says shouldApply false, status %d", data["applyStatus"])
     input("Press Ctrl-C to cancel, or enter to continue. ")
 
 
-
-mtkqcom = input("What is the SoC brand of the device you want to unlock? `mtk` for Mediatek, `qcom` for Qualcomm: ") # This is the worst way to implement this because i suck at everything
+# This is the worst way to implement this because i suck at everything
+mtkqcom = input(
+    "What is the SoC brand of the device you want to unlock? `mtk` for Mediatek, `qcom` for Qualcomm: ")
 
 if mtkqcom == "mtk":
     if use_fastboot:
@@ -50,10 +63,11 @@ if mtkqcom == "mtk":
         logging.debug("product is %s, token is %s", product, token)
 
     else:
-        product = input("Enter output from `fastboot getvar product` (Ctrl-C to cancel): ")
-        token = input("Enter output from `fastboot oem get_token` \nCombine the strings (left to right) then enter it here. (Ctrl-C to cancel): ")
+        product = input(
+            "Enter output from `fastboot getvar product` (Ctrl-C to cancel): ")
+        token = input(
+            "Enter output from `fastboot oem get_token` \nCombine the strings (left to right) then enter it here. (Ctrl-C to cancel): ")
         logging.debug("product is %s, token is %s", product, token)
-
 
 elif mtkqcom == "qcom":
     if use_fastboot:
@@ -62,60 +76,66 @@ elif mtkqcom == "qcom":
         logging.debug("product is %s, token is %s", product, token)
 
     else:
-        product = input("Enter output from `fastboot getvar product` (Ctrl-C to cancel): ")
-        token = input("Enter output from `fastboot getvar token` (Ctrl-C to cancel): ")
+        product = input(
+            "Enter output from `fastboot getvar product` (Ctrl-C to cancel): ")
+        token = input(
+            "Enter output from `fastboot getvar token` (Ctrl-C to cancel): ")
         logging.debug("product is %s, token is %s", product, token)
-
 
 else:
     print("Invalid answer.")
     exit()
 
 
-
 r = UnlockRequest(auth, "unlock.update.miui.com", "/api/v2/unlock/device/clear", {
-  "appId":"1",
-  "data":{
-    "clientId":"1",
-    "clientVersion":"6.5.406.31",
-    "language":"en",
-    "pcId":auth.pcid,
-    "product":product,
-    "region":"",
-}})
+    "appId": "1",
+    "data": {
+        "clientId": "1",
+        "clientVersion": "6.5.406.31",
+        "language": "en",
+        "pcId": auth.pcid,
+        "product": product,
+        "region": "",
+    }})
 r.add_nonce()
 data = r.run()
 logging.debug(data)
-print(f"Xiaomi server says: {data['notice']} It says that the unlock will {'' if data['cleanOrNot'] else 'not '}wipe data.")
+
+print(
+    f"Xiaomi server says: {data['notice']} It says that the unlock will {'' if data['cleanOrNot'] else 'not '}wipe data.")
 input("Press Ctrl-C to cancel, or enter to continue. ")
 
+
 r = UnlockRequest(auth, "unlock.update.miui.com", "/api/v3/ahaUnlock", {
-  "appId":"1",
-  "data":{
-    "clientId":"2",
-    "clientVersion":"6.5.406.31",
-    "language":"en",
-    "operate":"unlock",
-    "pcId":auth.pcid,
-    "product":product,
-    "region":"",
-    "deviceInfo":{
-      "boardVersion":"",
-      "product":product,
-      "socId":"",
-      "deviceName":""
-    },
-    "deviceToken":token,
-}})
+    "appId": "1",
+    "data": {
+        "clientId": "2",
+        "clientVersion": "6.5.406.31",
+        "language": "en",
+        "operate": "unlock",
+        "pcId": auth.pcid,
+        "product": product,
+        "region": "",
+        "deviceInfo": {
+            "boardVersion": "",
+            "product": product,
+            "socId": "",
+            "deviceName": ""
+        },
+        "deviceToken": token,
+    }})
 r.add_nonce()
 data = r.run()
 logging.debug(data)
 
+
 if use_fastboot:
-    confirm = input("Acquired token! Do you want to unlock now? Enter `yes` or `no`:")
+    confirm = input(
+        "Acquired token! Do you want to unlock now? Enter `yes` or `no`: ")
 
     if confirm == "yes":
-        fdev.Download(io.BytesIO(bytes.fromhex(data["encryptData"])), len(bytes.fromhex(data["encryptData"])))
+        fdev.Download(io.BytesIO(bytes.fromhex(data["encryptData"])), len(
+            bytes.fromhex(data["encryptData"])))
         time.sleep(0.5)
         fdev.Oem("unlock")
 
--- request.py
+++ request.py
@@ -1,45 +1,70 @@
 # Python 3.7+ only.
 
-import requests, webbrowser, json, logging, hmac, random, binascii, hashlib, urllib, uuid
-import http.cookiejar as cookielib
+import requests
+import json
+import logging
+import hmac
+import random
+import binascii
+import hashlib
+import urllib
+
 from urllib3.util.url import Url
 from base64 import b64encode, b64decode
 from Cryptodome.Cipher import AES
 from strings import STRINGS
 
+
 class XiaomiError(RuntimeError):
     def __init__(self, message, code):
         super().__init__(f"\33[31m[ERROR]\33[0m: {message}")
         self.code = code
+
+
 class UserError(XiaomiError):
     pass
 
-pad = lambda s: s + (16 - len(s) % 16) * bytes([16 - len(s) % 16])
-unpad = lambda s: s[:-s[-1]]
+
+def pad(s): return s + (16 - len(s) % 16) * bytes([16 - len(s) % 16])
+def unpad(s): return s[:-s[-1]]
+
+
 class Auth():
-    LOGIN_URL="" # removed since the url doesnt function as intended
-    START="&&&START&&&"
+    START = "&&&START&&&"
+
     def login_tui(self, sid):
-        self.login(input("Go to account.xiaomi.com,log in and paste the code inside code.txt\n(dont forget to change the username and password or else it will fail)\nto devtools console (press f12 then select console) then copypaste the response to here: ".format(self.LOGIN_URL.format(sid)))) # please edit this,this is so bad
+        self.login(input("Go to \33[34mhttps://account.xiaomi.com\33[0m, log in to your account, paste the code from code.txt (don't forget to change the username and password values here) to your browser DevTool console (press F12), and copy-paste the response here: "))
+
     def login(self, data):
         if data[:len(self.START)] != self.START:
-            raise UserError("invalid data (missing or invalid &&& section)", 1)
+            raise UserError("Invalid data (missing or invalid &&& section)", 1)
         try:
             data = json.loads(data[len(self.START):])
         except:
-            raise XiaomiError("invalid json but valid &&& start, probably internal error or changed format", 2)
+            raise XiaomiError(
+                "Invalid JSON data, but valid &&& start, probably internal error or changed format", 2)
+
         logging.debug(data)
         if data["code"] != 0:
             if data["code"] == 70016:
-                raise UserError("Invalid username or password,find the username and password section in code.txt and change them to your username and password.", 3)
+                raise UserError(
+                    "Invalid username or password, find username and password values in code.txt and change them to correct values (as a username you should be put your phone number or email, not your id).", 3)
             else:
-                raise XiaomiError("Account server gave unknown code {}, chinese desc is {}".format(data["code"], data["desc"]), 4)
-        elif "notificationUrl" in data and data["notificationUrl"].startswith("https://account.xiaomi.com/identity/authStart"):
-            raise UserError(f"You need to verify your Xiaomi account before beeing able to retreve valid information from servers. Open this link to start verification process: \33[34m{data['notificationUrl']}\33[0m", 3)
+                raise XiaomiError("Account server gave unknown code {}, english desc is {}".format(
+                    data["code"], data["descEN"]), 4)
+        elif "notificationUrl" in data and data["notificationUrl"].startswith("https://account.xiaomi.com/identity/authStart") and not "…" in data["notificationUrl"]:
+            raise UserError(
+                f"You need to verify your Xiaomi account before beeing able to retreve valid information from servers. Open this link to start verification process: \33[34m{data['notificationUrl']}\33[0m", 3)
+        elif "notificationUrl" in data and "…" in data["notificationUrl"]:
+            raise UserError(
+                "The response has been shortened by your browser DevTool, and because of that, it isn't valid. To fix that, check a quick guide here: \33[34mhttps://github.com/Canny1913/miunlock/pull/8#issuecomment-1186278739\33[0m", 3)
         elif "…" in data["location"]:
-            raise UserError("Your response has been shortened by the browser's DevTool, and because of that, it isn't valid. To fix that, check a quick guide here: \33[34mhttps://github.com/Canny1913/miunlock/pull/8#issuecomment-1186278739\33[0m", 3)
+            raise UserError(
+                "The response has been shortened by your browser DevTool, and because of that, it isn't valid. To fix that, check a quick guide here: \33[34mhttps://github.com/Canny1913/miunlock/pull/8#issuecomment-1186278739\33[0m", 3)
         elif data["location"] == "":
-            raise XiaomiError("Location URL is empty. This probably means that you've got an error or some sort of notice. Create a issue with full response here, so it can be investigated (but before posting, check for ssecurity, psecurity, userId, cUserId or passToken, and if they are present, censor them): \33[34mhttps://github.com/Canny1913/miunlock/issues/new\33[0m", 5)
+            raise XiaomiError(
+                "Location URL is empty. This probably means that you've got an error or some sort of notice. Create a issue with full response here, so it can be investigated (but before posting, check for ssecurity, psecurity, userId, cUserId or passToken, and if they are present, censor them): \33[34mhttps://github.com/Canny1913/miunlock/issues/new\33[0m", 5)
+
         self.ssecurity = data["ssecurity"]
         self.psecurity = data["psecurity"]
         self.userid = data["userId"]
@@ -47,19 +72,27 @@ class Auth():
         self.code = data["code"]
         self.nonce = data["nonce"]
         self.location = data["location"]
-        sign = urllib.parse.quote_plus(b64encode(hashlib.sha1(b"nonce="+str(self.nonce).encode("utf-8")+b"&"+self.ssecurity.encode("utf-8")).digest()))
+
+        sign = urllib.parse.quote_plus(b64encode(hashlib.sha1(
+            b"nonce="+str(self.nonce).encode("utf-8")+b"&"+self.ssecurity.encode("utf-8")).digest()))
         session = requests.Session()
         response = session.get(self.location + "&clientSign=" + sign)
         self.cookies = session.cookies
-        logging.debug("got cookies from auth redir %s", self.cookies)
+
+        logging.debug("Got cookies from auth redir:\n%s", self.cookies)
         if response.status_code == 401:
-            raise UserError("Sign in failed,don't reuse the same output,use a fresh one", 4)
+            raise UserError(
+                "Sign in failed. Don't reuse the same response, create a fresh one.", 4)
+
         response.raise_for_status()
-        logging.debug("auth redir head %s", response.headers)
-        logging.debug("auth redir text %s", response.text)
-        logging.debug("auth data %s", self.__dict__)
+        logging.debug("Auth redir head: %s", response.headers)
+        logging.debug("Auth redir text: %s", response.text)
+        logging.debug("Auth data: %s", self.__dict__)
+
         if response.json()["S"] != "OK":
-            raise XiaomiError("redir gave not-ok json", 5)
+            raise XiaomiError(
+                f"Redir returned not-ok result: {response.json()['S']}", 5)
+
         if self.location[37:39] == "wb":
             self.pcid = self.location[37:76]
         elif self.location[37:39] == "bl":
@@ -67,17 +100,22 @@ class Auth():
         else:
             pcid_end = self.location.index("&ticket")
             raise UserError(f"Unknown pcId: {self.location[37:pcid_end]}", 5)
+
         return True
 
+
 class UnlockRequest:
-    IV=b"0102030405060708"
-    DEFAULT_KEY=bytes.fromhex("327442656f45794a54756e6d57554771376251483241626e306b324e686875724f61714266797843754c56676e3441566a3773776361776535337544556e6f")
+    IV = b"0102030405060708"
+    DEFAULT_KEY = bytes.fromhex(
+        "327442656f45794a54756e6d57554771376251483241626e306b324e686875724f61714266797843754c56676e3441566a3773776361776535337544556e6f")
+
     def __init__(self, auth, host, path, params, method="POST"):
         self.auth = auth
         self.host = host
         self.path = path
         self.params = {}
-        for k,v in params.items():
+
+        for k, v in params.items():
             if isinstance(v, str):
                 v = v.encode("utf-8")
             elif not isinstance(v, bytes):
@@ -85,50 +123,78 @@ class UnlockRequest:
             if isinstance(k, str):
                 k = k.encode("utf-8")
             self.params[k] = v
+
         self.method = method
         logging.debug(self.params)
-        self.cipher = AES.new(b64decode(self.auth.ssecurity), AES.MODE_CBC, iv=self.IV)
+        self.cipher = AES.new(
+            b64decode(self.auth.ssecurity), AES.MODE_CBC, iv=self.IV)
+
     def get_params(self, sep):
-        logging.debug(self.method.encode("utf-8")+sep+self.path.encode("utf-8")+sep+b"&".join([k+b"="+v for k,v in self.params.items()]))
-        return self.method.encode("utf-8")+sep+self.path.encode("utf-8")+sep+b"&".join([k+b"="+v for k,v in self.params.items()])
+        params = self.method.encode("utf-8")+sep+self.path.encode(
+            "utf-8")+sep+b"&".join([k+b"="+v for k, v in self.params.items()])
+        logging.debug(params)
+
+        return params
+
     def add_sign(self):
-        self.params[b"sign"] = binascii.hexlify(hmac.digest(self.DEFAULT_KEY, self.get_params(b"\n"), "sha1"))
+        self.params[b"sign"] = binascii.hexlify(hmac.digest(
+            self.DEFAULT_KEY, self.get_params(b"\n"), "sha1"))
+
     def _encrypt(self, value):
         return b64encode(AES.new(b64decode(self.auth.ssecurity), AES.MODE_CBC, iv=self.IV).encrypt(pad(value)))
+
     def encrypt(self):
-        for k,v in self.params.items():
+        for k, v in self.params.items():
             logging.debug(k)
             logging.debug(v)
             self.params[k] = self._encrypt(v)
+
     def add_signature(self):
-        self.params[b"signature"] = b64encode(hashlib.sha1(self.get_params(b"&")+b"&"+self.auth.ssecurity.encode("utf-8")).digest())
+        self.params[b"signature"] = b64encode(hashlib.sha1(
+            self.get_params(b"&")+b"&"+self.auth.ssecurity.encode("utf-8")).digest())
+
     def add_nonce(self):
-        r = UnlockRequest(self.auth, self.host, "/api/v2/nonce", {"r":''.join(random.choices(list("abcdefghijklmnopqrstuvwxyz"), k=16)), "sid":"miui_unlocktool_client"}).run()
-        logging.debug("nonce is "+r["nonce"])
+        r = UnlockRequest(self.auth, self.host, "/api/v2/nonce", {"r": ''.join(random.choices(
+            list("abcdefghijklmnopqrstuvwxyz"), k=16)), "sid": "miui_unlocktool_client"}).run()
+        logging.debug("Nonce is "+r["nonce"])
+
         self.params[b"nonce"] = r["nonce"].encode("utf-8")
         self.params[b"sid"] = b"miui_unlocktool_client"
+
     def _decrypt(self, value):
         # Decrypt is only called once, but we can't do decryption after encryption on a cipher object. So remake it. Not too much overhead I hope.
-        ret = b64decode(unpad(AES.new(b64decode(self.auth.ssecurity), AES.MODE_CBC, iv=self.IV).decrypt(b64decode(value))))
-        logging.debug("query returned %s", ret)
+        ret = b64decode(unpad(AES.new(b64decode(self.auth.ssecurity),
+                        AES.MODE_CBC, iv=self.IV).decrypt(b64decode(value))))
+        logging.debug("Query returned %s", ret)
+
         return ret
+
     def run(self):
         self.add_sign()
         self.encrypt()
         self.add_signature()
         logging.debug(self.params)
+
         data = json.loads(self.send())
+
         if data.get("code", 0) != 0:
-            logging.error("invalid code != 0: %s", data)
-            raise XiaomiError(STRINGS["en"].get(data.get("code", -1), STRINGS["en"][-1]).format(**data), data.get("code", 6))
-#            raise XiaomiError("Invalid code {}".format(data.get("code", None)), 6)
+            logging.error("Invalid code != 0: %s", data)
+            raise XiaomiError(STRINGS["en"].get(
+                data.get("code", -1), STRINGS["en"][-1]).format(**data), data.get("code", 6))
+            #raise XiaomiError("Invalid code {}".format(data.get("code", None)), 6)
+
         return json.loads(self.send())
+
     def send(self):
-        response = requests.request(self.method, Url(scheme="https", host=self.host, path=self.path).url, data=self.params, headers={"User-Agent":"XiaomiPCSuite"}, cookies=self.auth.cookies)
+        response = requests.request(self.method, Url(scheme="https", host=self.host, path=self.path).url, data=self.params, headers={
+                                    "User-Agent": "XiaomiPCSuite"}, cookies=self.auth.cookies)
         logging.debug(response)
         logging.debug(response.headers)
+
         response.raise_for_status()
         logging.debug(response.text)
+
         data = self._decrypt(response.text)
-        logging.debug("query returned %s", data.decode("utf-8"))
+        logging.debug("Query returned %s", data.decode("utf-8"))
+
         return data
